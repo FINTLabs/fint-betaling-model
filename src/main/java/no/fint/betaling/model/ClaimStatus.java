ba0c79a3939541679b4b72b41ed18cdef52a74ec
torgravels <torleif@fintlabs.no>
2019-12-12 12:18:08 +0100

    More claim statuses

--- src/main/java/no/fint/betaling/model/ClaimStatus.java
+++ src/main/java/no/fint/betaling/model/ClaimStatus.java
@@ -1,7 +1,7 @@
 package no.fint.betaling.model;
 
 public enum ClaimStatus {
-    STORED("stored"), SENT("sent"), PAID("paid"), ERROR("error"), SEND_ERROR("send error"), UPDATE_ERROR("update error");
+    STORED("stored"), SENT("sent"), ACCEPTED("accepted"), PAID("paid"), ERROR("error"), SEND_ERROR("send error"), ACCEPT_ERROR("accept error"), UPDATE_ERROR("update error");
 
     private final String claimStatus;
 
